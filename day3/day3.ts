8e041549f524a6e997a4beed5f4164ced68f023b
Anja Bertels <anja.bertels@th-koeln.de>
2025-11-14 14:25:40 +0100

    solution for day 3

--- day3/day3.ts
+++ day3/day3.ts
@@ -1,27 +1,50 @@
 const input = await Deno.readTextFile("input.txt");
 const paths = input.split("\n").map(str => str.split(",").map(str => [str[0], parseInt(str.substring(1))]));
-console.log(part1())
+part1and2()
 
-function part1() {
-    const histories: Set<string>[] = []
-    for (const p of paths) {
-        histories.push(calculatePath(p, [0, 0]))
+function part1and2() {
+    const pathMap = calculatePath(paths[0]);
+    let minDistance = Infinity;
+    let minSteps = Infinity;
+    let path2 = [0, 0]
+    let step = 0;
+    for (const [direction, distance] of paths[1]) {
+        for (let i = 1; i <= distance; i++) {
+            step++;
+            switch (direction) {
+                case "U":
+                    path2[1]--
+                    break;
+                case "D":
+                    path2[1]++
+                    break;
+                case "L":
+                    path2[0]--
+                    break;
+                case "R":
+                    path2[0]++
+                    break;
+            }
+            const key = path2.toString();
+            if (pathMap.has(key)) {
+                const distance = Math.abs(path2[0]) + Math.abs(path2[1]);
+                minDistance = Math.min(minDistance, distance);
+                const path1Step = pathMap.get(key)!;
+                const steps = path1Step + step;
+                minSteps = Math.min(minSteps, steps);
+            }
+        }
     }
-    let intersections = histories[0].intersection(histories[1]);
-    return Math.min(
-        ...Array.from(intersections.values())
-            .map(str => {
-                const[x, y] = str.split(",").map(str => parseInt(str, 10));
-                return Math.abs(x) + Math.abs(y);
-            })
-    );
+    console.log(minDistance, minSteps)
 }
 
-function calculatePath(instructions: (string | number)[][], coordinates: number[]): Set<string> {
-    let path = [...coordinates];
-    const history: Set<string> = new Set();
+function calculatePath(instructions: (string | number)[][]): Map<string, number> {
+    let path = [0, 0];
+    const history: Map<string, number> = new Map();
+    let step = 0;
     for (const [direction, distance] of instructions) {
         for (let i = 1; i <= distance; i++) {
+            step++;
             switch (direction) {
                 case "U":
                     path[1]--
@@ -36,7 +59,10 @@ function calculatePath(instructions: (string | number)[][], coordinates: number[
                     path[0]++
                     break;
             }
-            history.add(path.toString())
+            const key = path.toString();
+            if (!history.has(key)) {
+                history.set(key, step)
+            }
         }
     }
     return history;
