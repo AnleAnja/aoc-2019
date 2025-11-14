76566a8f1584cf1e0ec393591ee0838e4a630ab1
Anja Bertels <anja.bertels@th-koeln.de>
2025-11-14 14:46:04 +0100

    day 4 part 2

--- day4/day4.ts
+++ day4/day4.ts
@@ -1,23 +1,30 @@
 const input = await Deno.readTextFile("input.txt");
 const min = Number(input.split("-")[0]);
 const max = Number(input.split("-")[1]);
-console.log(part1());
+console.log(part1and2());
 
-function part1() {
+function part1and2() {
     let options = 0;
     for (let i = min; i <= max; i++) {
         const s = String(i);
         let hasAdjacentDuplicates = false;
         let isNeverDecreasing = true;
-        for (let j = 1; j < s.length; j++) {
-            let previous = s[j - 1];
+        let j = 0;
+        while (j < s.length) {
+            if (j > 0 && s[j] < s[j - 1]) {
+                isNeverDecreasing = false;
+                break;
+            }
             let current = s[j];
-            if (previous === current) {
+            let streak = 1;
+            while (j + 1 < s.length && s[j + 1] === current) {
+                streak++;
+                j++;
+            }
+            if (streak === 2) {
                 hasAdjacentDuplicates = true;
             }
-            if (previous > current) {
-                isNeverDecreasing = false;
-            }
+            j++;
         }
         if (hasAdjacentDuplicates && isNeverDecreasing) {
             options++;
@@ -25,4 +32,3 @@ function part1() {
     }
     return options;
 }
-
